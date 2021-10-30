ad70a579543b2b0336a2fbfec56de494a0a7c3cb
Pitcode <89965684+codes13@users.noreply.github.com>
2021-10-30 18:45:41 +0530

    Update videoplayer.js

--- videoplayer.js
+++ videoplayer.js
@@ -1,7 +1,7 @@
 var player = videojs('videoPlayer',{
 // autoplay : "muted",
  controls  : true,
-
+poster : 'https://codes13.github.io/foundersdayposter.png' , 
 fluid : true, 
 // aspectRatio : '4:3'
 playbackRates : [0.25,0.5,1,1.5,2 ],
