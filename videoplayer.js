fbbeeb0dbeb20764b05fcc28a24e6ec17ea89f46
Pitcode <89965684+codes13@users.noreply.github.com>
2021-10-30 18:39:35 +0530

    Add files via upload

--- videoplayer.js
+++ videoplayer.js
@@ -1,10 +1,10 @@
 var player = videojs('videoPlayer',{
 // autoplay : "muted",
  controls  : true,
-poster: "https://picsum.photos/800/450",
+
 fluid : true, 
 // aspectRatio : '4:3'
-playbackRates : [0.25,0.5,1,1.5 , 2 , 2.5 , 3 , 2.5 , 4 , 4.0 ],
+playbackRates : [0.25,0.5,1,1.5,2 ],
 VideoPlaybackQuality : true, 
 
   
@@ -13,10 +13,10 @@ VideoPlaybackQuality : true,
 });
 
 player.watermalk({
-  file : 'https://picsum.photos/50/50',
+  file : '',
   xpos:0,
   ypos: 0,
   opacity:1,
   clickable :true,
-  url : 'https://picsum.photos/50/50'
+  url : ''
 })
